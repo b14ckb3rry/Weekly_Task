e4b3e90e4e0ec0827c13f6d78a97dfd1469a7370
Md Sirajul Islam <127536373+b14ckb3rry@users.noreply.github.com>
2023-10-15 19:38:32 +0600

    Add files via upload

---
+++ Week_03/A_XORwice.cpp
@@ -0,0 +1,25 @@
+//***********************************//
+//*         MD SIRAJUL ISLAM        *//
+//*           DEPT OF CSE           *//
+//*              BAUET              *//
+//*      github.com/b14ckb3rry      *//
+//***********************************//
+
+#include <bits/stdc++.h>
+using namespace std;
+#define maru 0;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin>>t;
+    while(t--){
+        long long a,b; cin>>a>>b;
+        long long res=a^b;
+        cout<<res<<'\n';
+    }
+    
+    return maru
+}

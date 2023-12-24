e1e397d432f040fee9f5a5044c0b9b5764cd1d0c
Md. Sirajul Islam <127536373+b14ckb3rry@users.noreply.github.com>
2023-12-24 21:50:22 +0600

    Add files via upload

---
+++ Week 13 Super Zone/K_Building_an_Aquarium.cpp
@@ -0,0 +1,62 @@
+//***********************************//
+//*         MD SIRAJUL ISLAM        *//
+//*      github.com/b14ckb3rry      *//
+//***********************************//
+
+#include <bits/stdc++.h>
+using namespace std;
+#define maru 0;
+typedef long long ll;
+#ifdef ONLINE_JUDGE
+#define debug(...) 101
+#define debug_array(x, y) 101
+#else
+#include "blackBox.h"
+#endif
+
+bool ailaJadu(ll arr[], ll n, ll mid, ll x)
+{
+    ll sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < mid)
+        {
+            sum += (mid - arr[i]);
+        }
+    }
+    return sum <= x;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        ll arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        ll right = 0, left = INT_MAX, max_height = INT_MAX;
+        while (right <= left)
+        {
+            ll mid = (left - right) / 2 + right;
+            if (ailaJadu(arr, n, mid, x))
+            {
+                max_height = mid;
+                right = mid + 1;
+            }
+            else
+            {
+                left = mid - 1;
+            }
+        }
+        cout << max_height << '\n';
+    }
+
+    return maru
+}
